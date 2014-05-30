827dc6d563b15a2ab196ce48d0c059a53a1bcdb3
lesBoloss <and1homecourt@mbpdemaeuducret.home>
2014-05-30 08:42:15 +0200

    adding files

---
+++ angularAPIinsta/js.js
@@ -0,0 +1,39 @@
+  var app = angular.module("GridCambiable", []);
+
+	app.factory('instagram', ['$http', function($http)
+	{
+		return {
+			fetchPopular: function(callback)
+			{
+	            var endPoint = "https://api.instagram.com/v1/users/3/media/recent/?client_id=7e20239c982b49e58cb52532e30d1cfd&callback=JSON_CALLBACK";
+
+	            $http.jsonp(endPoint).success(function(response)
+	            {
+	                callback(response.data);
+	            });
+			}
+		}
+	}]);
+	
+	app.controller('GridCambiableController', ['$scope', 'instagram' ,
+	function ($scope, instagram)
+	{
+		$scope.layout = 'grid';
+	    
+	    $scope.setLayout = function(layout)
+	    {
+	        $scope.layout = layout;
+	    };
+	    
+	    $scope.isLayout = function(layout)
+	    {
+	        return $scope.layout == layout;
+	    };
+	
+		$scope.pics = [];
+	
+		instagram.fetchPopular(function(data)
+		{
+			$scope.pics = data;
+		});
+	}]);
